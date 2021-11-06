42cc25b5328cd2559b6458a1a31da6d873a7cb6d
liuxiaowei-1001 <taniyafghyu786@gmail.com>
2021-11-07 00:41:48 +0800

    three

--- src/main/java/com/bonc/sprinboot01/demo/Ceshi.java
+++ src/main/java/com/bonc/sprinboot01/demo/Ceshi.java
@@ -5,5 +5,6 @@ public class Ceshi {
     public static void main(String[] args) {
         System.out.println("pull");
         System.out.println("Ceshi.main");
+        System.out.println("Ceshi.main2");
     }
 }
