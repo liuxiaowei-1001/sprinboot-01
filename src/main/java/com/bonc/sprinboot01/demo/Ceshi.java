3af056dabed7b6bc13dc9ec24b0264f4cb4677b4
lxw <taniyafghyu786@gmail.com>
2021-11-07 00:21:05 +0800

    Update Ceshi.java

--- src/main/java/com/bonc/sprinboot01/demo/Ceshi.java
+++ src/main/java/com/bonc/sprinboot01/demo/Ceshi.java
@@ -3,6 +3,6 @@ package com.bonc.sprinboot01.demo;
 public class Ceshi {
 
     public static void main(String[] args) {
-        System.out.println("我真的醉了");
+        System.out.println("pull");
     }
 }
